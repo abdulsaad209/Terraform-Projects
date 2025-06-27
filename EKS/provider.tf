2fc770e7d23f550ffe8ad56e9b790160d06c0b98
saadzahid <saadzahid248@gmail.com>
2025-06-28 03:13:45 +0500

    push

---
+++ EKS/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+    backend "s3" {
+      bucket = "bucket-for-terraform-state-file-saad248"
+      region = "us-east-1"
+      key = "terraform.tfstate"
+      dynamodb_table = "terraform-eks-state-locks"
+    }
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "6.0.0-beta3"
+    }
+  }
+}
+
+provider "aws" {
+  region = "${var.aws-region}"
+  access_key = "${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_KEY}"
+}
+
