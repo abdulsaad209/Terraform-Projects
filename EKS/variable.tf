f26607132194a0371fcee48d5ac0687e85fd6da3
saad <saadzahid248@gmail.com>
2025-07-01 01:21:59 +0500

    change the region settings

--- EKS/variable.tf
+++ EKS/variable.tf
@@ -11,7 +11,7 @@ variable "aws-region" {
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
@@ -23,7 +23,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "private_subnet_cidrs" {
@@ -63,7 +63,7 @@ variable "node_groups" {
   }))
   default = {
     general = {
-      instance_types = ["t3.medium"]
+      instance_types = ["t2.micro"]
       capacity_type  = "ON_DEMAND"
       scaling_config = {
         desired_size = 2
