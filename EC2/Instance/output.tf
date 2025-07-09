4276dad5598ba9a23e9f8f9346ad62431ce0bd6d
Saad Zahid <saad.zahid@zurple.com>
2025-07-10 01:28:47 +0500

    push

--- EC2/Instance/output.tf
+++ EC2/Instance/output.tf
@@ -4,9 +4,9 @@
 #
 #
 
-output "PUBLIC_IP_OF_UBUNTU_SERVER" {
-  value = aws_instance.webserver.public_ip
-}
+# output "PUBLIC_IP_OF_UBUNTU_SERVER" {
+#   value = aws_instance.webserver.public_ip
+# }
 
 output "key-name" {
   value = aws_key_pair.tf_key.key_name
