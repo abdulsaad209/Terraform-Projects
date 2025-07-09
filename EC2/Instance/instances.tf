04943ae4583b0c3d8ee552d6e5a48308ded139a7
saad <saadzahid248@gmail.com>
2025-07-10 03:22:17 +0500

    push

--- EC2/Instance/instances.tf
+++ EC2/Instance/instances.tf
@@ -5,14 +5,14 @@ resource "aws_key_pair" "tf_key" {
 }
 
 # Create a VPC
-module "portfolio_vpc" {
-  source = "modules/vpc"
+module "vpc" {
+  source = "./modules/vpc"
 
   vpc_cidr             = var.vpc_cidr
   availability_zones   = var.availability_zones
   private_subnet_cidrs = var.private_subnet_cidrs
   public_subnet_cidrs  = var.public_subnet_cidrs
-  cluster_name         = var.app_name
+  app_name         = var.app_name
 }
 
 # Create a security group to allow inbound traffic on specified ports
